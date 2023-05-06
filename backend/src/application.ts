95e3d734fa9b01754c3557ee3b4b9719b7d8176b
Donald Cull <20510711+donaldCull@users.noreply.github.com>
2023-05-06 10:12:25 +0100

    add middleware for logging, cors and add handler for /

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,9 +1,18 @@
 import * as express from 'express';
+import * as morgan from 'morgan';
+import * as cors from 'cors';
 import { CareRecipientController } from './controllers/CareRecipientController';
 import { CareGiverController } from './controllers/CareGiverController';
 import { CareEventController } from './controllers/CareEventController';
 
 const app = express();
+
+app.use(morgan('combined'));
+app.use(cors());
+
+app.get('/', (_req, res) => {
+  res.status(200).json('/');
+});
 
 app.use(CareRecipientController);
 app.use(CareGiverController);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,5 +4,6 @@ const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
+  // eslint-disable-next-line no-console
   console.log(`Server started at http://localhost:${port}`);
 });
