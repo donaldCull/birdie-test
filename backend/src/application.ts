466f14ac71d14def2a29047b3858079aaf8d73d1
Donald Cull <20510711+donaldCull@users.noreply.github.com>
2023-05-12 08:43:43 +0100

    backend hardening and build config

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,14 +1,24 @@
 import * as express from 'express';
 import * as morgan from 'morgan';
 import * as cors from 'cors';
+import helmet from "helmet";
+import RateLimit from 'express-rate-limit';
 import { CareRecipientController } from './controllers/CareRecipientController';
 import { CareGiverController } from './controllers/CareGiverController';
 import { CareEventController } from './controllers/CareEventController';
 
 const app = express();
 
+app.use(helmet());
 app.use(morgan('dev'));
 app.use(cors());
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 app.get('/', (_req, res) => {
   res.status(200).json('/');
