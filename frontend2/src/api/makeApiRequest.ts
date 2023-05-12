0eda0f864f048c9fe55b8b7398aeb53aaebc9f38
Donald Cull <20510711+donaldCull@users.noreply.github.com>
2023-05-12 13:12:21 +0100

    handle env and deployment issues

--- frontend2/src/api/makeApiRequest.ts
+++ frontend2/src/api/makeApiRequest.ts
@@ -1,5 +1,6 @@
 export default async function makeApiRequest(endpoint: string) {
-  const response = await fetch(`http://localhost:8000/${endpoint}`);
+  const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8000/' : 'http://don-test.eu-west-1.elasticbeanstalk.com/';
+  const response = await fetch(`${baseUrl}${endpoint}`);
   const jsonData = await response.json();
   return jsonData;
 }
